19f961d2f60c5a8323a8c2aa78121c7dd14eb6cb
akmanthesuperhero <arunakhil2@gmail.com>
2016-06-29 12:43:12 -0700

    Added LBP and Mask

--- slic.py
+++ slic.py
@@ -16,13 +16,28 @@ import cv2
 import collections
 import csv
  
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
+
+
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required = True, help = "Path to the image")
+ap.add_argument("-j", "--mask", required = True, help = "Path to mask")
 args = vars(ap.parse_args())
+args2 = vars(ap.parse_args())
  
 # load the image and convert it to a floating point data type
 image = img_as_float(io.imread(args["image"]))
+maskimage = img_as_float(io.imread(args2["mask"]))
+
+#greyimage = Image.open(args["image"]).convert('LA')
+#agreyimage = np.asarray(greyimage)
+
+
+gray = rgb2gray(image)    
+
  
 # loop over the number of segments
 #for numSegments in (100, 200, 300):
@@ -45,8 +60,7 @@ plt.show()
 file = open("supersize.txt", "w")
 counter = 0
 
-dict = collections.Counter()
-dict2 = collections.Counter()
+
 
 
 #for (i, segVal) in enumerate(np.unique(segments)):
@@ -65,8 +79,14 @@ dict2 = collections.Counter()
         #image2 = Image.fromarray(pixs)
         #colors = image2.getcolors(maxcolors=256) 
 
-        
+
 #get size and average color of each superpixel
+
+dict = collections.Counter()
+dict2 = collections.Counter()
+dict3 = collections.Counter()
+dict4 = collections.Counter()
+dict5 = collections.Counter()
 
 xr = len(image)
         #print xr
@@ -79,16 +99,42 @@ for i in range(0, xr):
                 #print j
         pixel = image[i, j]
         lbl = segments[i, j]
+
         dict2[lbl] += 1
         dict[lbl] += pixel
+        dict5[lbl] += maskimage[i,j]
+        if (i > 0 and j > 0 and i < xr-1 and j < yr-1):
+            code = 0
+            code |= int(gray[i-1,j-1] > gray[i,j]) << 7
+            code |= int(gray[i-1,j] > gray[i,j]) << 6
+            code |= int(gray[i-1,j+1] > gray[i,j]) << 5
+            code |= int(gray[i,j+1] > gray[i,j]) << 4
+            code |= int(gray[i+1,j+1] > gray[i,j]) << 3
+            code |= int(gray[i+1,j] > gray[i,j]) << 2
+            code |= int(gray[i+1,j-1] > gray[i,j]) << 1
+            code |= int(gray[i,j-1] > gray[i,j]) << 0
+            dict3[lbl] += code
+            dict4[lbl] += 1
 
-print image[0, 0]
-print image[60, 60]
-print "DONE DONE DONE"
+
+maskdict = collections.Counter()
+
+for i in range(0, len(dict5)):
+    if (dict5[i] / dict2[i] > 0.5):
+        maskdict[i] = 1
+        
+
+
+
+
+
 
 w = csv.writer(open("output.csv", "w"))
 for key, val in dict.items():
-    w.writerow([key, val[0]/(dict2[key]), val[1]/(dict2[key]) , val[2]/(dict2[key]), dict2[key]] )
+    w.writerow([key, val[0]/(dict2[key]), val[1]/(dict2[key]) , val[2]/(dict2[key]), dict3[key]/dict4[key], dict2[key], maskdict[key]] )
+
+
+
 
 
 
