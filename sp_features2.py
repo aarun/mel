46a0102cd413fadfe7d6fc0a6906219be613ecdc
Sahana-Srinivasan <sahanas009@gmail.com>
2016-07-06 16:59:53 -0700

    CSV features
    Generates area, centroid location, rgb, glcm properties, and distance
    from center. Generates 3000 super pixels out of original image.

--- sp_features2.py
+++ sp_features2.py
@@ -1,8 +1,19 @@
-# Test to read Part 2 super pixel images
+#Creates a CSV file with features for each superpixel
+#Currently contains: superpixel label, area, centroid location, RGB, and 5 GLCM properties
+#(GLCM properties: dissimilarity, correlation, contrast, energy, homogeneity)
 
 import numpy as np
 import os
 from skimage.measure import regionprops
+from skimage.feature import greycomatrix, greycoprops
+from skimage import data
+from skimage.io import imread
+from PIL import Image
+from skimage.segmentation import slic
+from math import sqrt
+
+
+PATCH_SIZE = 10
 
 def decodeSuperpixelIndex(rgbValue):
     """
@@ -18,6 +29,9 @@ def decodeSuperpixelIndex(rgbValue):
 # This may be used as:
 from PIL import Image
 
+def rgb2gray(rgb):
+
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.114])
 
 
 #for fn in os.listdir('.') :
@@ -35,14 +49,28 @@ imarr_dec = decodeSuperpixelIndex(imarr_enc)
 
 original = Image.open('ISIC_0000000.jpg')
 imarr_orig = np.array(original)
+imarr_bw = rgb2gray(imarr_orig)
+
+segments = slic(original, n_segments = 3000, sigma = 5, slic_zero = 2)
+
 
 sp_dict = {}
 
-for (i, segVal) in enumerate(np.unique(imarr_dec)) :
+both = {} # distance in both directions
+
+
+xr = len(imarr_orig)
+yr = len(imarr_orig[0])
+
+    
+centerx = xr/2
+centery = yr/2
+
+for (i, segVal) in enumerate(np.unique(segments)) :
 #for (i, segVal) in enumerate([0,1]):	
-	mask = np.zeros(imarr_dec.shape[:2], dtype='uint8')
-	mask[imarr_dec == segVal] = 255
-	area = len(mask[imarr_dec == segVal])	
+	mask = np.zeros(segments.shape[:2], dtype='uint8')
+	mask[segments == segVal] = 255
+	area = len(mask[segments == segVal])	
 	sp_locations = mask[:,:] == 255
 
 
@@ -50,36 +78,76 @@ for (i, segVal) in enumerate(np.unique(imarr_dec)) :
 	g = (sum(imarr_orig[sp_locations,1]))/area
 	b = (sum(imarr_orig[sp_locations,2]))/area
 
-	#print 'Average color: ', r, g, b
 
-
-	#print " superpixel %d" % (i)
-	#print 'Superpixel: ', i, 'Num regions: ', len(props), props[0].centroid, props[0].area
 	props = regionprops(mask, cache=True )
-	sp_dict[segVal] = [props[0].centroid, area, r, g, b]
-	if props[0].area != len(mask[imarr_dec == segVal]):
-	    print 'NO'
-   
 
-# at this point the sp_dict dictionary - has a list of all the unique superpixel labels
-# and also - how many pixels are in each superpixel
-#total_pix = 0
-#for i in range(len(sp_dict)) :
-#    total_pix += sp_dict[i]
+	centroid_loc = [0,0]
+	centroid_loc[0] = (int)(props[0].centroid[0])
+	centroid_loc[1] = (int)(props[0].centroid[1])
+	patch = imarr_bw[centroid_loc[0]:centroid_loc[0] + PATCH_SIZE, centroid_loc[1]:centroid_loc[1] + PATCH_SIZE]
 
-#print fn, 'SP: ', imarr_dec.shape, imarr_dec.min(), imarr_dec.max(), imarr_dec.shape[0]*imarr_dec.shape[1]
+	glcm = greycomatrix(patch, [1], [0], 256, symmetric=True, normed=True)
+	dissimilarity = greycoprops(glcm, 'dissimilarity')[0,0]
+	correlation =  greycoprops(glcm, 'correlation')[0,0]
+	contrast = greycoprops(glcm, 'contrast')[0,0]
+	energy = greycoprops(glcm, 'energy')[0,0]
+	homogeneity = greycoprops(glcm, 'homogeneity')[0,0]
 
-dict_str = 'Superpixel label, Centroid row, Centroid column, Area, Avg R value, Avg G value, Avg B value' + '\n'
-#f_out.write(str(sp_dict))
+	distance = sqrt( abs(props[0].centroid[0] - (xr/2))**2 + abs(props[0].centroid[1] - (yr/2))**2 )
+
+
+	sp_dict[segVal] = [props[0].centroid, area, r, g, b, dissimilarity, correlation, contrast, energy, homogeneity, distance]
+	
+
+dict_str = ('Superpixel label, Centroid row, Centroid column, Area,'
+	+ ' Avg R value, Avg G value, Avg B value, Dissimilarity, Correlation,'
+	+ ' Contrast, Energy, Homogeneity, Distance from center' + '\n')
 
 for k in sp_dict:
-    dict_str += (str(k) + ', ' + str(int(sp_dict[k][0][0])) + ', ' + str(int(sp_dict[k][0][1])) + ', ' 
-    + str(int(sp_dict[k][1])) + ', ' + str(sp_dict[k][2]) + ', ' + str(sp_dict[k][3]) + ', ' + str(sp_dict[k][4]) + '\n')
+    dict_str += (str(k) + ', ' + str(int(sp_dict[k][0][0])) + ', ' 
+    	+ str(int(sp_dict[k][0][1])) + ', ' + str(int(sp_dict[k][1]))
+    	 + ', ' + str(sp_dict[k][2]) + ', ' + str(sp_dict[k][3])
+    	  + ', ' + str(sp_dict[k][4])+ ', ' + str(sp_dict[k][5]) + ', ' 
+    	  + str(sp_dict[k][6]) + ', ' + str(sp_dict[k][7]) + ', ' 
+    	  + str(sp_dict[k][8]) + ', ' + str(sp_dict[k][9])  + ', ' + str(sp_dict[k][10]) + '\n')
 
 f_out.write(dict_str)
 f_out.close()
 
-#print sp_dict
+imarr_diss = np.zeros((len(imarr_dec),len(imarr_dec[0])), dtype=np.uint8 )
+imarr_corr = np.zeros((len(imarr_dec),len(imarr_dec[0])), dtype=np.uint8 )
+imarr_cont = np.zeros((len(imarr_dec),len(imarr_dec[0])), dtype=np.uint8 )
+imarr_energy = np.zeros((len(imarr_dec),len(imarr_dec[0])), dtype=np.uint8 )
+imarr_homo =  np.zeros((len(imarr_dec),len(imarr_dec[0])), dtype=np.uint8 )
+
+for row in range(imarr_diss.shape[0]) :
+	for col in range(imarr_diss.shape[1]) :
+		imarr_diss[row][col] = int((sp_dict[segments[row][col]][5])*(25500/367))
+		corr = int((sp_dict[segments[row][col]][6])*255)
+		if corr < 0:
+			corr = 0
+		imarr_corr[row][col] = corr
+		cont = int((sp_dict[segments[row][col]][7])*255)
+		if cont > 255:
+			cont = 255
+		imarr_cont[row][col] = cont
+		imarr_energy[row][col] = int((sp_dict[segments[row][col]][8])*255)
+		imarr_homo[row][col] = int((sp_dict[segments[row][col]][9])*255)
+
+
+		
+img = Image.fromarray(imarr_diss)
+img2 = Image.fromarray(imarr_corr)
+img3 = Image.fromarray(imarr_cont)
+img4 = Image.fromarray(imarr_energy)
+img5 = Image.fromarray(imarr_homo)
+img.show()
+img2.show()
+img3.show()
+img4.show()
+img5.show()
+
+
 
 
 
