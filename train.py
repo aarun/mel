0b67cf5805a672d1d70a9cea9b7e33d12317252d
akmanthesuperhero <arunakhil2@gmail.com>
2016-07-09 14:50:46 -0700

    Added list and platform functionality

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import csv
 import cPickle
 from sklearn.externals import joblib
 import os
+from sys import platform as _platform
 
 ap = argparse.ArgumentParser()
 ap.add_argument('-n', '--number', required = False, help = 'number of files')
@@ -21,20 +22,89 @@ if (args['number'] != None) :
 	number = int(args['number'])
 
 
+file_list =[]
 
-#print args
+if (args['list'] != None) :
+	with open(args['list']) as batch_file :
+		for line in batch_file :
+			a = line.strip('\n')
+			file_list.append(a)
 
-data = []
-groundtruth = []
-counter = 0
-seg_gt_dir = '/Users/18AkhilA/Documents/mel/ISBI2016_ISIC_Part1_Training_GroundTruth'
-for fn in os.listdir('.') :
-	if (fn.endswith('.txt')) :
-		if (number != None) : 
-			if (counter < number) :
+
+	#print args
+
+	data = []
+	groundtruth = []
+	counter = 0
+	if (_platform == "darwin") : 
+		seg_gt_dir = '/Users/18AkhilA/Documents/mel/ISBI2016_ISIC_Part1_Training_GroundTruth'
+	else :
+		seg_gt_dir = 'C:\mel\ISBI2016_ISIC_Part1_Training_GroundTruth'
+	for fn in file_list :
+		if (fn.endswith('.txt')) :
+			if (number != None) : 
+				if (counter < number) :
+
+					
+
+					print 'loading ' + fn
+
+					input_file = csv.DictReader(open(fn))
+					#print input_file.fieldnames
+
+					r = []
+					g = []
+					b = []
+
+					dis = []
+					corr = []
+					con = []
+					en = []
+					hom = []
+					dtc = []
+
+
+					for row in input_file:
+						r.append(float(row[" Avg R value"]))
+						g.append(float(row[" Avg G value"]))
+						b.append(float(row[" Avg B value"]))
+
+						dis.append(float(row[" Dissimilarity"]))
+						corr.append(float(row[" Correlation"]))
+						con.append(float(row[" Contrast"]))
+						en.append(float(row[" Energy"]))
+						hom.append(float(row[" Homogeneity"]))
+						dtc.append(float(row[" Distance from center"]))
+
+					temp = zip(r, g, b, dis, corr, con, en, hom, dtc)
+
+					data.extend(temp)
+
+					fn2 = fn.replace('.txt', '_Segmentation.txt')
+
+					if (_platform == "darwin") : 
+						long_fn = seg_gt_dir + "/" + fn2
+					else :
+						long_fn = seg_gt_dir + '\\' + fn2
+
+					ground_file = csv.DictReader(open(long_fn))
+
+					#print ground_file.fieldnames
+
+					m = [0]
+
+					for row in ground_file:
+						m.append(int(row[" mask0"]))
+
+					groundtruth.extend(m)
+					counter += 1
 
 				
 
+
+
+			else :
+				
 				print 'loading ' + fn
 
 				input_file = csv.DictReader(open(fn))
@@ -69,7 +139,10 @@ for fn in os.listdir('.') :
 				data.extend(temp)
 
 				fn2 = fn.replace('.txt', '_Segmentation.txt')
-				long_fn = seg_gt_dir + "/" + fn2
+				if (_platform == "darwin") : 
+						long_fn = seg_gt_dir + "/" + fn2
+				else :
+					long_fn = seg_gt_dir + '\\' + fn2
 
 				ground_file = csv.DictReader(open(long_fn))
 
@@ -81,82 +154,173 @@ for fn in os.listdir('.') :
 					m.append(int(row[" mask0"]))
 
 				groundtruth.extend(m)
-				counter += 1
-
-			
 
 
-
-		else :
-			
-			print 'loading ' + fn
-
-			input_file = csv.DictReader(open(fn))
-			#print input_file.fieldnames
-
-			r = []
-			g = []
-			b = []
-
-			dis = []
-			corr = []
-			con = []
-			en = []
-			hom = []
-			dtc = []
+					
 
 
-			for row in input_file:
-				r.append(float(row[" Avg R value"]))
-				g.append(float(row[" Avg G value"]))
-				b.append(float(row[" Avg B value"]))
 
-				dis.append(float(row[" Dissimilarity"]))
-				corr.append(float(row[" Correlation"]))
-				con.append(float(row[" Contrast"]))
-				en.append(float(row[" Energy"]))
-				hom.append(float(row[" Homogeneity"]))
-				dtc.append(float(row[" Distance from center"]))
+				#print fn
+				#temp = np.loadtxt(fn, delimiter= ",", skiprows=1)
 
-			temp = zip(r, g, b, dis, corr, con, en, hom, dtc)
+				#temp = np.delete(temp, (0), axis = 1)
+				#temp = np.delete(temp, (0), axis = 1)
+				#temp = np.delete(temp, (0), axis = 1)
+				#temp = np.delete(temp, (0), axis = 1)
+				#data.append(temp)
+				#fn2 = fn.replace('.txt', '_Segmentation.txt')
+				#long_fn = seg_gt_dir + "/" + fn2
+				
+				#grnd = np.loadtxt(long_fn, dtype = int,delimiter= ',', skiprows=1, usecols=[0])
 
-			data.extend(temp)
+				#grnd = np.delete(grnd, (0), axis = 1)
+				#groundtruth.append(grnd)
 
-			fn2 = fn.replace('.txt', '_Segmentation.txt')
-			long_fn = seg_gt_dir + "/" + fn2
+else :
 
-			ground_file = csv.DictReader(open(long_fn))
+	data = []
+	groundtruth = []
+	counter = 0
+	if (_platform == "darwin") : 
+		seg_gt_dir = '/Users/18AkhilA/Documents/mel/ISBI2016_ISIC_Part1_Training_GroundTruth'
+	else :
+		seg_gt_dir = 'C:\mel\ISBI2016_ISIC_Part1_Training_GroundTruth'
+	for fn in os.listdir('.') :
+		if (fn.endswith('.txt')) :
+			if (number != None) : 
+				if (counter < number) :
 
-			#print ground_file.fieldnames
+					
 
-			m = [0]
+					print 'loading ' + fn
 
-			for row in ground_file:
-				m.append(int(row[" mask0"]))
+					input_file = csv.DictReader(open(fn))
+					#print input_file.fieldnames
 
-			groundtruth.extend(m)
+					r = []
+					g = []
+					b = []
 
+					dis = []
+					corr = []
+					con = []
+					en = []
+					hom = []
+					dtc = []
+
+
+					for row in input_file:
+						r.append(float(row[" Avg R value"]))
+						g.append(float(row[" Avg G value"]))
+						b.append(float(row[" Avg B value"]))
+
+						dis.append(float(row[" Dissimilarity"]))
+						corr.append(float(row[" Correlation"]))
+						con.append(float(row[" Contrast"]))
+						en.append(float(row[" Energy"]))
+						hom.append(float(row[" Homogeneity"]))
+						dtc.append(float(row[" Distance from center"]))
+
+					temp = zip(r, g, b, dis, corr, con, en, hom, dtc)
+
+					data.extend(temp)
+
+					fn2 = fn.replace('.txt', '_Segmentation.txt')
+					if (_platform == "darwin") : 
+						long_fn = seg_gt_dir + "/" + fn2
+					else :
+						long_fn = seg_gt_dir + '\\' + fn2
+
+					ground_file = csv.DictReader(open(long_fn))
+
+					#print ground_file.fieldnames
+
+					m = [0]
+
+					for row in ground_file:
+						m.append(int(row[" mask0"]))
+
+					groundtruth.extend(m)
+					counter += 1
 
 				
 
 
 
-			#print fn
-			#temp = np.loadtxt(fn, delimiter= ",", skiprows=1)
+			else :
+				
+				print 'loading ' + fn
 
-			#temp = np.delete(temp, (0), axis = 1)
-			#temp = np.delete(temp, (0), axis = 1)
-			#temp = np.delete(temp, (0), axis = 1)
-			#temp = np.delete(temp, (0), axis = 1)
-			#data.append(temp)
-			#fn2 = fn.replace('.txt', '_Segmentation.txt')
-			#long_fn = seg_gt_dir + "/" + fn2
+				input_file = csv.DictReader(open(fn))
+				#print input_file.fieldnames
+
+				r = []
+				g = []
+				b = []
+
+				dis = []
+				corr = []
+				con = []
+				en = []
+				hom = []
+				dtc = []
+
+
+				for row in input_file:
+					r.append(float(row[" Avg R value"]))
+					g.append(float(row[" Avg G value"]))
+					b.append(float(row[" Avg B value"]))
+
+					dis.append(float(row[" Dissimilarity"]))
+					corr.append(float(row[" Correlation"]))
+					con.append(float(row[" Contrast"]))
+					en.append(float(row[" Energy"]))
+					hom.append(float(row[" Homogeneity"]))
+					dtc.append(float(row[" Distance from center"]))
+
+				temp = zip(r, g, b, dis, corr, con, en, hom, dtc)
+
+				data.extend(temp)
+
+				fn2 = fn.replace('.txt', '_Segmentation.txt')
+				if (_platform == "darwin") : 
+						long_fn = seg_gt_dir + "/" + fn2
+				else :
+					long_fn = seg_gt_dir + '\\' + fn2
+
+				ground_file = csv.DictReader(open(long_fn))
+
+				#print ground_file.fieldnames
+
+				m = [0]
+
+				for row in ground_file:
+					m.append(int(row[" mask0"]))
+
+				groundtruth.extend(m)
+
+
+					
+
+
+
+				#print fn
+				#temp = np.loadtxt(fn, delimiter= ",", skiprows=1)
+
+				#temp = np.delete(temp, (0), axis = 1)
+				#temp = np.delete(temp, (0), axis = 1)
+				#temp = np.delete(temp, (0), axis = 1)
+				#temp = np.delete(temp, (0), axis = 1)
+				#data.append(temp)
+				#fn2 = fn.replace('.txt', '_Segmentation.txt')
+				#long_fn = seg_gt_dir + "/" + fn2
+				
+				#grnd = np.loadtxt(long_fn, dtype = int,delimiter= ',', skiprows=1, usecols=[0])
+
+				#grnd = np.delete(grnd, (0), axis = 1)
+				#groundtruth.append(grnd)
 			
-			#grnd = np.loadtxt(long_fn, dtype = int,delimiter= ',', skiprows=1, usecols=[0])
-
-			#grnd = np.delete(grnd, (0), axis = 1)
-			#groundtruth.append(grnd)
-		
+			
 
 print 'TRAINING'
 forest = RandomForestRegressor(n_estimators = 500, n_jobs = 1)
