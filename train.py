48e7bcb2dc9647b2ec5143aaa4f3c56f6d186edd
akmanthesuperhero <arunakhil2@gmail.com>
2016-07-09 14:40:03 -0700

    Added specific number trainer and way to view output of forest

--- train.py
+++ train.py
@@ -11,14 +11,84 @@ import cPickle
 from sklearn.externals import joblib
 import os
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-n', '--number', required = False, help = 'number of files')
+ap.add_argument('-l', '--list', required = False, help = 'name of batch file')
+args = vars(ap.parse_args())
+#args2 = vars(ap.parse_args())
+number = None
+if (args['number'] != None) :
+	number = int(args['number'])
+
+
+
+#print args
+
 data = []
 groundtruth = []
 counter = 0
 seg_gt_dir = '/Users/18AkhilA/Documents/mel/ISBI2016_ISIC_Part1_Training_GroundTruth'
 for fn in os.listdir('.') :
 	if (fn.endswith('.txt')) :
-		if (counter < 7):
+		if (number != None) : 
+			if (counter < number) :
 
+				
+
+				print 'loading ' + fn
+
+				input_file = csv.DictReader(open(fn))
+				#print input_file.fieldnames
+
+				r = []
+				g = []
+				b = []
+
+				dis = []
+				corr = []
+				con = []
+				en = []
+				hom = []
+				dtc = []
+
+
+				for row in input_file:
+					r.append(float(row[" Avg R value"]))
+					g.append(float(row[" Avg G value"]))
+					b.append(float(row[" Avg B value"]))
+
+					dis.append(float(row[" Dissimilarity"]))
+					corr.append(float(row[" Correlation"]))
+					con.append(float(row[" Contrast"]))
+					en.append(float(row[" Energy"]))
+					hom.append(float(row[" Homogeneity"]))
+					dtc.append(float(row[" Distance from center"]))
+
+				temp = zip(r, g, b, dis, corr, con, en, hom, dtc)
+
+				data.extend(temp)
+
+				fn2 = fn.replace('.txt', '_Segmentation.txt')
+				long_fn = seg_gt_dir + "/" + fn2
+
+				ground_file = csv.DictReader(open(long_fn))
+
+				#print ground_file.fieldnames
+
+				m = [0]
+
+				for row in ground_file:
+					m.append(int(row[" mask0"]))
+
+				groundtruth.extend(m)
+				counter += 1
+
+			
+
+
+
+		else :
+			
 			print 'loading ' + fn
 
 			input_file = csv.DictReader(open(fn))
@@ -86,7 +156,7 @@ for fn in os.listdir('.') :
 
 			#grnd = np.delete(grnd, (0), axis = 1)
 			#groundtruth.append(grnd)
-		counter += 1
+		
 
 print 'TRAINING'
 forest = RandomForestRegressor(n_estimators = 500, n_jobs = 1)
---
+++ viewer.py
@@ -0,0 +1,45 @@
+from sklearn.ensemble import RandomForestRegressor
+import numpy as np
+from PIL import Image
+import argparse
+from skimage import io
+from skimage.segmentation import slic
+from skimage.segmentation import mark_boundaries
+from skimage.util import img_as_float
+import csv
+import cPickle
+from sklearn.externals import joblib
+import os
+
+
+seg_gt_dir = '/Users/18AkhilA/Documents/mel/ISBI2016_ISIC_Part1_Training_Data'
+
+for fn in os.listdir('.') :
+	if (fn.endswith('.csv')) :
+		
+		input_file = np.loadtxt(fn, delimiter= ",")
+		fn2 = fn.replace('_Prediction.csv', '.jpg')
+		print "Displaying Prediction of " + fn2
+		long_fn = seg_gt_dir + "/" + fn2
+
+		image = Image.open(long_fn)
+		imarr_image = np.array(image)
+		imarr_mask = np.zeros((imarr_image.shape[0], imarr_image.shape[1]))
+		segments = slic(image, n_segments = 3000, sigma = 5, slic_zero = 2)
+
+		for (i, segVal) in enumerate(np.unique(segments)) :
+
+			val = input_file[segVal]
+
+			if (val < 0.5) :
+				imarr_mask[segments == segVal] = 0
+			else :
+				imarr_mask[segments == segVal] = 255
+
+
+		im = Image.fromarray(imarr_mask)
+		im.show()
+
+
+
+			
