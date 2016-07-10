e4c8f24befdc097ee874fa5d2133c2b8d44dcf42
parthasrinivasan <partha@srinivasans.us>
2016-07-10 14:48:50 -0700

    Added batch file support
    Modified to check errors and print accuracy for an entire batch of
    files. Image display commented out.

--- checker.py
+++ checker.py
@@ -1,86 +1,109 @@
 import numpy as np
 from skimage.segmentation import slic
 from PIL import Image
+from sys import platform as _platform
+import argparse
 
+ap = argparse.ArgumentParser()
+ap.add_argument('-l', '--list', required = True, help = 'name of batch file')
+args = vars(ap.parse_args())
+
+
+file_list =[]
+
+with open(args['list']) as batch_file :
+	for line in batch_file :
+		if line.endswith('.jpg\n') :
+			line = line.replace('.jpg', '.txt')
+		a = line.strip('\n')
+		file_list.append(a)
+
+#print file_list
 
 #stnd = np.loadtxt("output6.csv", delimiter= ",")
+print 'image              , accuracy, sensitivity, specificity, dice_coeff, jaccard_ind'
+for fn in file_list:
+    if fn.endswith('.txt'):
 
-prediction = np.loadtxt("ISIC_0000089_Prediction.csv", delimiter= ",")
 
-falsepos = 0
-falseneg = 0
-trueneg = 0
-truepos = 0
+		root_name = fn.strip('.txt')
+		#print 'Processing file root: ', root_name		
+		prediction = np.loadtxt(root_name+'_Prediction.csv', delimiter= ",")
 
-gt = Image.open('ISIC_0000089_Segmentation.png')
-original = Image.open('/Users/sahana/Mel/ISBI2016_ISIC_Part1_Training_Data/ISIC_0000089.jpg')
-imarr_gt = np.array(gt)
-imarr_image = np.array(original)
-imarr_predict = np.zeros((imarr_image.shape[0], imarr_image.shape[1]))
-segments = slic(original, n_segments = 3000, sigma = 5, slic_zero = 2)
-imarr_overlay = np.array(original)
+		falsepos = 0
+		falseneg = 0
+		trueneg = 0
+		truepos = 0
 
-for (i, segVal) in enumerate(np.unique(segments)) :
-
-		val = prediction[segVal]
-
-		if (val < 0.5) :
-			imarr_predict[segments == segVal] = 0
+		if (_platform == "darwin") : 
+			seg_gt_dir = '/users/sahana/mel/ISBI2016_ISIC_Part1_Training_GroundTruth'
+			orig_dir = '/Users/sahana/Mel/ISBI2016_ISIC_Part1_Training_Data/'
+			gt_fn = seg_gt_dir + "/" + root_name + '_Segmentation.png'			
 		else :
-			imarr_predict[segments == segVal] = 255
+			seg_gt_dir = 'C:\mel\ISBI2016_ISIC_Part1_Training_GroundTruth'
+			orig_dir = 'C:\mel\ISBI2016_ISIC_Part1_Training_Data\\'			
+			gt_fn = seg_gt_dir + "\\" + root_name + '_Segmentation.png'			
+
+		gt = Image.open(gt_fn,'r')
+		original = Image.open(orig_dir + root_name + '.jpg')
+		imarr_gt = np.array(gt)
+		imarr_image = np.array(original)
+		imarr_predict = np.zeros((imarr_image.shape[0], imarr_image.shape[1]))
+		segments = slic(original, n_segments = 3000, sigma = 5, slic_zero = 2)
+		imarr_overlay = np.array(original)
+
+		for (i, segVal) in enumerate(np.unique(segments)) :
+
+				val = prediction[segVal]
+
+				if (val < 0.5) :
+					imarr_predict[segments == segVal] = 0
+				else :
+					imarr_predict[segments == segVal] = 255
 
 
-for i in range(len(imarr_gt)) :
-	for j in range(len(imarr_gt[i])) :
-		if (imarr_gt[i][j] == 255):
-			if (imarr_predict[i][j] == 0) :
-				falseneg += 1
-				imarr_overlay[i][j] = [255,0,0]
-			else :
-				truepos += 1
-				imarr_overlay[i][j] = [255,255,255]
-		else :
-			if (imarr_predict[i][j] == 0) :
-				trueneg += 1
-				imarr_overlay[i][j] = [0,0,0]
-			else :
-				falsepos += 1
-				imarr_overlay[i][j] = [0,255,0]
+		for i in range(len(imarr_gt)) :
+			for j in range(len(imarr_gt[i])) :
+				if (imarr_gt[i][j] == 255):
+					if (imarr_predict[i][j] == 0) :
+						falseneg += 1
+						imarr_overlay[i][j] = [255,0,0]
+					else :
+						truepos += 1
+						imarr_overlay[i][j] = [255,255,255]
+				else :
+					if (imarr_predict[i][j] == 0) :
+						trueneg += 1
+						imarr_overlay[i][j] = [0,0,0]
+					else :
+						falsepos += 1
+						imarr_overlay[i][j] = [0,255,0]
 
-print truepos
-print falseneg
-print trueneg
-print falsepos
+		#print truepos
+		#print falseneg
+		#print trueneg
+		#print falsepos
 
-tp = float(truepos)
-tn = float(trueneg)
-fp = float(falsepos)
-fn = float(falseneg)
+		tp = float(truepos)
+		tn = float(trueneg)
+		fp = float(falsepos)
+		fn = float(falseneg)
 
-accuracy = (tp + tn)/(tp + fp + tn + fn)
-sensitivity = tp/(tp + fn)
-specificity = tn/(tn + fp)
-dice_coeff = (2*tp)/((2*tp) + fn + tp)
-jaccard_ind = tp/(tp + fn + fp)
+		accuracy = (tp + tn)/(tp + fp + tn + fn)
+		sensitivity = tp/(tp + fn)
+		specificity = tn/(tn + fp)
+		dice_coeff = (2*tp)/((2*tp) + fn + tp)
+		jaccard_ind = tp/(tp + fn + fp)
 
-image = Image.fromarray(imarr_overlay)
-image.show()
+		#image = Image.fromarray(imarr_overlay)
+		#image.show()
 
-#for i in range(len(stnd)):
-#	if (stnd[i] == 1):
-#		if (chck[i] != 1):
-#			falseneg += 1 
-#	if (stnd[i] == 1):
-#		#print "do"
-#		if (chck[i] == 0):
-#			
-#			falsepos+= 1
-
-print accuracy
-print sensitivity
-print specificity
-print dice_coeff
-print jaccard_ind
+		print root_name, accuracy, sensitivity, specificity, dice_coeff, jaccard_ind
+		#print accuracy
+		#print sensitivity
+		#print specificity
+		#print dice_coeff
+		#print jaccard_ind
 
 
 
