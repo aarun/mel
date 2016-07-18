dda39a20be6814f23cd331faab182d8ef38e21aa
parthasrinivasan <partha@srinivasans.us>
2016-07-18 12:06:10 -0700

    Revert "p3 updates"
    This reverts commit 8cb1bdec0f00e0daba5674caaafa47930740764e.

--- p3_train.py
+++ p3_train.py
@@ -38,39 +38,35 @@ else :
 
 data = []
 groundtruth = []
-
+groundtruth2 = []
 counter = 0
 if (_platform == "darwin") : 
-	gt_fn = '/Users/sahana/Mel/mel/ISBI2016_ISIC_Part3_Training_GroundTruth.csv'
+	gt_fn = '/Users/18AkhilA/Documents/mel/ISBI2016_ISIC_Part3_Training_GroundTruth.csv'
 else :
-	gt_fn = 'C:\mel\ISBI2016_ISIC_Part3_Training_GroundTruth.csv'
+	gt_fn = 'C:\mel\ISBI2016_ISIC_Part2_Training_GroundTruth.csv'
 
 		
 
 for fn in file_list :
-	if (fn.endswith('.jpg')) :
+	if (fn.endswith('.txt')) :
 		if (number == None or counter < number) : 
 
 			print 'loading ' + fn
-			input_file = csv.DictReader(open(fn.replace('.jpg', '.txt')))
+			input_file = csv.DictReader(open(fn))
 
-			features = []
+			r = []
+			g = []
+			b = []
 
+			dis = []
+			corr = []
+			con = []
+			en = []
+			hom = []
 
-			for row in input_file:
-				r = []
-				g = []
-				b = []
-
-				dis = []
-				corr = []
-				con = []
-				en = []
-				hom = []
-
-				dtc = []
-				nrow = []
-				ncol = []
+			dtc = []
+			nrow = []
+			ncol = []
 
 
 			for row in input_file:
@@ -88,23 +84,23 @@ for fn in file_list :
 				nrow.append(float(row[" Normalized row"]))
 				ncol.append(float(row[" Normalized column"]))
 
-			temp = [r, g, b, dis, corr, con, en, hom, dtc, nrow, ncol]
 
-			data.append(temp)
+			temp = zip(r, g, b, dis, corr, con, en, hom, dtc, nrow, ncol)
+			data.extend(temp)
+
+			ground_file = csv.DictReader(gt_fn)
+
+			m = []
+
+			for row in ground_file :
+				if (row[1] == 'benign') :
+					m.append(1)
+				else :
+					m.append(0)
+
+			groundtruth.extend(m)
 			counter += 1
 
-m = []
-
-with open(gt_fn) as gt :
-	ground_file = csv.DictReader(gt)
-	for row in ground_file :
-		if (row["label"] == 'benign') :
-			m.append(int(0))
-		else :
-			m.append(int(1))
-
-groundtruth.extend(m)
-print 'Label length: ' + str(len(groundtruth))
 	
 print 'TRAINING'
 forest = RandomForestRegressor(n_estimators = 500, n_jobs = 8)
@@ -117,3 +113,5 @@ if (_platform == "darwin") :
 else :
 	with open('c:\mel\\forest_part3.pkl', 'wb') as f:
 	    cPickle.dump(forest, f)
+
+	  
